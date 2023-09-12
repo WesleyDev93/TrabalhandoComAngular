7aaf4d27e128a73414e3f855c45cf7d0be3e395b
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-09-12 10:26:20 -0300

    Usando metodo oninit

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent {
+
+// criando interface do OnInit
+// a interface espera um metodo
+
+export class TitleComponent implements OnInit {
+
+  // Criando tratamento do metodo
+
+  nome: string = 'Wesley Oliveira';
+
+  constructor () {}
+
+
+
+  // interface de inicialização da classe
+  // com isso podemos ao inicalizar o componente determinar uma ação logo de inicio
+  // conforme a minha necessidade
+
+
+  ngOnInit(): void {
+    this.nome = ` ola  ${this.nome}`
+  }
 
 }
