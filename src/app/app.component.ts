1809ad6f04bcce12aa66c19aab24e3b59103f3ea
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-09-12 13:43:22 -0300

    Usando OnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+
+  // Essa propriedade diz se ele esta vivo ou morto agora
+  // e atribuimos ela ao HTML
+  isAlive : boolean = true;
+
+  // Propriedade do botão
+
+  dispose(): void {
+
+    this.isAlive = false;
+  }
 }
--- src/app/check-sample/check-sample.component.ts
+++ src/app/check-sample/check-sample.component.ts
@@ -1,4 +1,5 @@
-import { Component,OnInit,DoCheck,AfterContentChecked,AfterContentInit,AfterViewChecked,AfterViewInit } from '@angular/core';
+import { Component,OnInit,DoCheck,AfterContentChecked
+  ,AfterContentInit,AfterViewChecked,AfterViewInit,OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-check-sample',
@@ -6,7 +7,32 @@ import { Component,OnInit,DoCheck,AfterContentChecked,AfterContentInit,AfterView
   styleUrls: ['./check-sample.component.css']
 })
 export class CheckSampleComponent implements OnInit,
-DoCheck,AfterContentChecked,AfterContentInit,AfterViewChecked,AfterViewInit {
+DoCheck,AfterContentChecked,AfterContentInit,AfterViewChecked,AfterViewInit,OnDestroy {
+   // vamos criar a propriedade quantidade que está no HTML do check-sample
+   // variavel quantidade inicial no zero
+
+   quantidade: number = 0;
+
+
+   // metodo de manuseio da variavel quantidade
+
+   adicionar () {
+
+    this.quantidade += 1;
+
+   }
+
+
+   decrementar () {
+
+    this.quantidade -= 1;
+
+   };
+
+
+
+   // metodo construtor deve existir para nossas interações
+  constructor () {}
 
 
   ngOnInit(): void {
@@ -15,17 +41,27 @@ DoCheck,AfterContentChecked,AfterContentInit,AfterViewChecked,AfterViewInit {
   ngDoCheck(): void {
     console.log ('DoCheck');
   }
+  // apos alguma alteração verifica o conteudo
   ngAfterContentChecked(): void {
     console.log ('AfterContentChecked');
   }
+
+    // quando o conteudo é iniciado
   ngAfterContentInit(): void {
     console.log ('AfterContentInit');
   }
+  // apos alguma alteração verifica a view
   ngAfterViewChecked(): void {
     console.log ('AfterViewChecked');
   }
+  // depois da inicialização da view
   ngAfterViewInit(): void {
     console.log ('AfterViewInit');
+  }
+ // para destruir algum componente
+  ngOnDestroy(): void {
+
+    console.log ('OnDestroy');
   }
 
   // vamos usar os metodos que estão acima no import
