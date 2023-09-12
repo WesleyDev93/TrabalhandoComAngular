6d9325c9a45b09bd1c39dc98704504f9d77aa1cc
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-09-12 14:01:25 -0300

    vendo a estrutura dos modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,37 @@
+// basico para trabalhar com  modulos
 import { NgModule } from '@angular/core';
+//basico para trabalhar com  modulos
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TituloComponente  } from './title/title.component';
 import { CheckSampleComponent } from './check-sample/check-sample.component';
 
+
+
+
+
+
 @NgModule({
+  // os componentes  devem ser declarados aqui e ser importados acima
   declarations: [
     AppComponent,
     TituloComponente,
     CheckSampleComponent,
   ],
+  // podemos importar os modulos aqui
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+
+  // vamos adicionar o exports ele serve para exportar nossos modulos
+  exports: [],
+  // camada de serviço
   providers: [],
+  // inicialização dos componentes
   bootstrap: [AppComponent]
 })
 export class AppModule { }
